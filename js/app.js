4af7c6e6620fb38edbd0a8d5120a4e23231f9482
Marko Antolić <mantolic@zagreb.hr>
2017-02-09 14:53:53 +0100

    fiddling with output

---
+++ js/app.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+
+  var finalHTML = "";
+
+  function displayData() {
+    $.each(JSONdata, function(i, streamer) {
+      console.log(JSONdata[i].stream);
+      if ( JSONdata[i].stream !== null || JSONdata[i].stream !== undefined) {
+
+      finalHTML += '<ul id="streamer-grid">';
+      finalHTML += '<li class="pure-u-1 pure-u-lg-1-3 one-streamer">';
+      finalHTML += '<img src="';
+      finalHTML += JSONdata[i].stream.logo;
+      finalHTML += '">';
+      finalHTML += '<span>';
+      finalHTML += JSONdata[i].stream.display_name;
+      finalHTML += '</span>';
+      finalHTML += '</ul>';
+
+      }
+
+      });
+
+
+    $('#streamer-list').html(finalHTML);
+  }
+
+  displayData();
+});
+
